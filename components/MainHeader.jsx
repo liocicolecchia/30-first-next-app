3a4ba3d903434921b711ccdcb055a393333e1956
Lio <liocicolecchia@gmail.com>
2026-07-10 21:18:49 +0200

    how to add styling in a next.js project with css modules

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 
+import classes from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
 
 function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
         <img src={logoImg.src} alt="A plate with food on it" />
         NextLevel Food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
