19ca780a8f1d4df23b6a68d525110f22ce28c618
Lio <liocicolecchia@gmail.com>
2026-07-11 18:59:21 +0200

    creating a lib folder with a meals.js where i connect to the db and do my db querys. making the getMeals() async and adding a Timeout to simulate a 2 seconds delay for easier developement loading states etc.

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/MealsGrid";
+import { getMeals } from "@/lib/meals";
 
-function MealsPage() {
+async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +19,7 @@ function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
