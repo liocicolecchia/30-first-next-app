b451cf6b7de729144555a6336090b1395a5a74df
Lio <liocicolecchia@gmail.com>
2026-07-12 15:01:21 +0200

    setting up the structure of the MealDetailsPage

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,8 +1,32 @@
+import Image from "next/image";
+
+import classes from "./page.module.css";
+
 function MealDetailsPage() {
   return (
-    <main>
-      <h1>Meal Detail Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: "...",
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
 
