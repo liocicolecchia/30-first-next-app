42e04f5029134049de834fffeec0c2fdb90b6733
Lio <liocicolecchia@gmail.com>
2026-07-12 18:47:26 +0200

    adjusting the ImagePicker because it looks bad. making sure that the input is not visible and adding a button below it. connecting the input to a ref and onClick of the button programaticly click the input field using the ref. also converting the component to a client component

--- components/meals/ImagePicker.jsx
+++ components/meals/ImagePicker.jsx
@@ -1,11 +1,31 @@
+"use client";
+
+import { useRef } from "react";
+
 import classes from "./ImagePicker.module.css";
 
 function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
-        <input type="file" id={name} accept="image/png, image/jpeg" name={name} />
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+        />
+        <button className={classes.button} type="button" onClick={handlePickClick}>
+          Pick an Image
+        </button>
       </div>
     </div>
   );
