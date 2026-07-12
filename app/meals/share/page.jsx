920a0066714ece0b4b6d3f3cd6872621de636afd
Lio <liocicolecchia@gmail.com>
2026-07-13 00:49:49 +0200

    a different way to handle errors from invalid form submissions. in shareMeal action returning a object with a message if invalid and using useFormState in the share page to display a <p> with the returned message. this dosnt load the error page and provides better UX. converting share page to a client component

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,9 +1,15 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
 import ImagePicker from "@/components/meals/ImagePicker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/MealsFormSubmit";
 
 function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +19,7 @@ function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -37,6 +43,7 @@ function ShareMealPage() {
             <textarea id="instructions" name="instructions" rows="10" required></textarea>
           </p>
           <ImagePicker label="Your Image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
