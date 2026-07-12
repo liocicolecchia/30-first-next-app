a60427020dfa2ce6cff9cd7a99048e93c8553cd5
Lio <liocicolecchia@gmail.com>
2026-07-12 15:16:27 +0200

    adding a fetch for the meal details to meals.js and outputing all details on MealDetailsPage

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,20 +1,27 @@
 import Image from "next/image";
 
 import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
 
-function MealDetailsPage() {
+async function MealDetailsPage({ params }) {
+  const { mealSlug } = await params;
+
+  const meal = getMeal(mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image fill />
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
-          <h1>TITLE</h1>
+          <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${"EMAIL"}`}>NAME</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={classes.summary}>SUMMARY</p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
 
@@ -22,7 +29,7 @@ function MealDetailsPage() {
         <p
           className={classes.instructions}
           dangerouslySetInnerHTML={{
-            __html: "...",
+            __html: meal.instructions,
           }}
         ></p>
       </main>
